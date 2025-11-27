252997e83b132b5462cf30b1e4bdd24437062d81
Patryk Rejent <82150867+Patryk244@users.noreply.github.com>
2025-11-27 14:23:33 +0100

    wywołanie procedury UpdateBestsellers z poziomu Javy

--- kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
+++ kodilla-jdbc/src/test/java/com/kodilla/jdbc/StoredProcTestSuite.java
@@ -1,5 +1,6 @@
 package com.kodilla.jdbc;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.sql.*;
@@ -9,10 +10,16 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class StoredProcTestSuite {
 
+    private DbManager dbManager;
+
+    @BeforeEach
+    void setUp() throws SQLException {
+        dbManager = DbManager.getInstance();
+    }
+
     @Test
     public void testUpdateVipLevels() throws SQLException {
         // Given
-        DbManager dbManager = DbManager.getInstance();
         String sqlUpdate = "UPDATE READERS SET VIP_LEVEL=\"Not set\"";
         Statement statement = dbManager.getConnection().createStatement();
         statement.executeUpdate(sqlUpdate);
@@ -31,6 +38,49 @@ public class StoredProcTestSuite {
         }
         assertEquals(0, howMany);
         rs.close();
+        statement.close();
+        statement2.close();
+    }
+
+    @Test
+    public void testUpdateBestsellers() throws SQLException {
+        // Given
+        Connection conn = dbManager.getConnection();
+        Statement statement = conn.createStatement();
+
+        String sqlUpdate = "UPDATE BOOKS SET BESTSELLER = 0";
+        statement.executeUpdate(sqlUpdate);
+
+        String sqlCheckBestsellers = "SELECT COUNT(*) AS BESTSELLERS_COUNT FROM BOOKS WHERE BESTSELLER = 1";
+        String sqlCountExpectedBestsellers = """
+        SELECT COUNT(DISTINCT BOOK_ID) AS EXPECTED_COUNT
+        FROM RENTS
+        GROUP BY BOOK_ID
+        HAVING COUNT(*) >= 2
+        """;
+
+        ResultSet rsExpected = statement.executeQuery(sqlCountExpectedBestsellers);
+        int expectedBestsellers = 0;
+        while (rsExpected.next()) {
+            expectedBestsellers++;
+        }
+        rsExpected.close();
+
+        // When
+        Statement statement2 = conn.createStatement();
+        String sqlProcedureCall = "CALL UpdateBestsellers()";
+        statement2.execute(sqlProcedureCall);
+
+        // Then
+        ResultSet rsActual = statement.executeQuery(sqlCheckBestsellers);
+        int actualBestsellersCount = -1;
+        if (rsActual.next()) {
+            actualBestsellersCount = rsActual.getInt("BESTSELLERS_COUNT");
+        }
+
+        assertEquals(expectedBestsellers, actualBestsellersCount);
+
+        rsActual.close();
         statement.close();
         statement2.close();
     }
